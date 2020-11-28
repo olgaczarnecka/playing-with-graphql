b677909a4a43a091af83f13544f18e6f668a6105
Olga Czarnecka <olga.czarnecka@lunarlogic.io>
2020-11-29 00:32:29 +0100

    Add nodemon and set database using json-server

--- schema/schema.js
+++ schema/schema.js
@@ -1,6 +1,5 @@
 const graphql = require('graphql');
-const _ = require('lodash');
-
+const axios = require('axios');
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
 
 const UserType = new GraphQLObjectType({
@@ -12,11 +11,6 @@ const UserType = new GraphQLObjectType({
   },
 });
 
-const users = [
-  { id: '23', firstName: 'Bill', age: 20 },
-  { id: '47', firstName: 'Samantha', age: 21 },
-];
-
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType', // it is what points us to a very particular record in database
   fields: {
@@ -24,7 +18,9 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } }, // if you're looking for a user and give me the id I'll give you back the user
       resolve(parentValue, args) {
-        return _.find(users, { id: args.id }); //lodash helper, walk through users and find first with given id
+        return axios
+          .get(`http://localhost:3000/users/${args.id}`)
+          .then((resp) => resp.data); // {data: {firstName: 'bill'}}
       },
     },
   },
